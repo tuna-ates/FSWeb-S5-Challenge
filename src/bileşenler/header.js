d24800b0cfa312006ba695d778a6dc4cb3d387e3
tuna-ates <tuna151ates@gmail.com>
2023-11-19 14:19:30 +0300

    header.js tarafı tamamlandı.

--- src/bileşenler/header.js
+++ src/bileşenler/header.js
@@ -10,6 +10,25 @@ const Header = (baslik, tarih, yazi) => {
   //    <h1>{ baslik }</h1>
   //    <span class="temp">{ yazi }</span>
   //  </div>
+      
+     const header=document.createElement("div");
+     header.classList.add("header");
+
+     const date=document.createElement("span");
+     date.classList.add("date");
+     date.textContent=tarih;
+     header.append(date);
+
+     const baslikH1=document.createElement("h1");
+     baslikH1.textContent=baslik;
+     header.append(baslikH1);
+
+     const info=document.createElement("span");
+     info.classList.add("temp");
+     info.textContent=yazi;
+     header.append(info);
+
+     return header;
   //
 }
 
@@ -20,10 +39,15 @@ const headerEkleyici = (secici) => {
   // Görev 1'de tanımladığınız Header bileşenini kullanarak bir header oluşturun (baslik,tarih,yazi parametrelerini kendi isteğinize göre belirleyin)
   // Oluşturulan header'i, verilen seçiciyle eşleşen DOM'daki öğeye eklemelidir.
   //
-
+    
   // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper")) 
   // fakat aynı zamanda bir değişken de alabilir (bknz: querySelector(secici))
  
+  const result= Header("DENEME BAŞLIĞI","10.05.2002","deneme deneme deneme deneme deneme deneme");
+    const htmlHeader=document.querySelector(secici);
+    htmlHeader.append(result);
+
+
 }
 
 export { Header, headerEkleyici }
